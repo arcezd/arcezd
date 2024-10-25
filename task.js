22a029a067c903d87160fa4b671cf0f633cf61ca
Diego Arce <diego@arce.cr>
2024-10-25 00:20:36 -0600

    fix(pdf): add resume pdf info
    Signed-off-by: Diego Arce <diego@arce.cr>

--- task.js
+++ task.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const USE_LOCAL_CHROME = process.env.USE_LOCAL_CHROME || 'false';
 
 const HTML_TEMPLATE_SUBFOLDER = process.env.HTML_TEMPLATE_SUBFOLDER || 'html/default';
-const RESUME_LANGUAGE = process.env.RESUME_LANGUAGE || 'en';
+const RESUME_LANGUAGE = process.env.RESUME_LANGUAGE || 'en-US';
 
 const TAG_REF_V = process.env.TAG_REF_V;
 const COMMIT_SHA = process.env.COMMIT_SHA;
@@ -33,13 +33,27 @@ async function renderResumeHtml() {
     const templateLang = JSON.parse(langTemplateJsonStr);
     let data = JSON.parse(dataStr);
 
-    // Keep only the last 5 jobs experience
+    // keep only the last 5 jobs experience
     data.experience = data.experience.slice(0, 5);
 
-    // Remove contributions for new resumes
+    // keep only the last 5 jobs experience
+    data.experience = data.experience.slice(0, 5);
+
+    // remove contributions for new resumes
     data.contributions = [];
 
-    // Load compilation data
+    // remove projects for new resumes
+    data.projects = [];
+
+    // remove education for new resumes
+    data.education = [];
+
+    // show only the last 5 skills
+    if (data.skills && data.skills.languages) data.skills.languages = data.skills.languages.slice(0, 5);
+    if (data.skills && data.skills.frameworks) data.skills.frameworks = data.skills.frameworks.slice(0, 5);
+    if (data.skills && data.skills.other) data.skills.other = data.skills.other.slice(0, 5);
+
+    // load compilation data
     if (TAG_REF_V && COMMIT_SHA && GITHUB_REPO_URL) {
       data = {
         ...data,
@@ -55,6 +69,50 @@ async function renderResumeHtml() {
     Handlebars.registerHelper('i18n', function (value) {
       if (templateLang[value]) return templateLang[value];
       else console.error(`Error mapping template 'i18n' variable for value: ${value}.`);
+    });
+
+    Handlebars.registerHelper('monthYearDate', function (value) {
+      if (value) {
+        // check if date is in ISO8601 format
+        if (!isNaN(Date.parse(value))) {
+          const date = new Date(value);
+          // parse date to short format
+          shortDate = date.toLocaleDateString(RESUME_LANGUAGE, { year: 'numeric', month: 'short' });
+          return shortDate;
+        } else if (value.toLowerCase() === 'today' || value.toLowerCase() === 'actual') {
+          return value;
+        }
+        else console.error(`Error mapping template 'shortDate' variable for value: ${value}.`);
+      }
+      else console.error(`Error mapping template 'shortDate' variable for value: ${value}.`);
+    });
+
+    Handlebars.registerHelper('shortDate', function (value) {
+      if (value) {
+        // check if date is in ISO8601 format
+        if (!isNaN(Date.parse(value))) {
+          const date = new Date(value);
+          // parse date to short format dd/MM/YY
+          dateString = date.toLocaleDateString(RESUME_LANGUAGE, { day: '2-digit', month: '2-digit', year: 'numeric' });
+          return dateString;
+        }
+        else console.error(`Error mapping template 'shortDate' variable for value: ${value}.`);
+      }
+      else console.error(`Error mapping template 'shortDate' variable for value: ${value}.`);
+    });
+
+    Handlebars.registerHelper('onlyYearDate', function (value) {
+      if (value) {
+        // check if date is in ISO8601 format
+        if (!isNaN(Date.parse(value))) {
+          const date = new Date(value);
+          // parse date to format year/month
+          dateString = date.toLocaleDateString(RESUME_LANGUAGE, { year: 'numeric' });
+          return dateString;
+        }
+        else console.error(`Error mapping template 'onlyYear' variable for value: ${value}.`);
+      }
+      else console.error(`Error mapping template 'onlyYear' variable for value: ${value}.`);
     });
 
     const template = Handlebars.compile(templateSrc);
